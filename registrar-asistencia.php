5c07ab0fcc1b01215dabe48c65e5f0b7056274b9
alerellan <alerellan@gmail.com>
2021-04-21 10:34:28 -0300

    control de marcacion de asistencia

--- registrar-asistencia.php
+++ registrar-asistencia.php
@@ -4,7 +4,18 @@ session_start();
 
 $usuario_id = $_SESSION['usuario_id'];
 
-echo date('d-m-Y');
+$query ="select docentes.id from docentes, usuarios where usuario_id = '$usuario_id'";
+$consulta = mysqli_query($conexion,$query);
+$array = mysqli_fetch_assoc($consulta);
+$id_docente= $array['id'];
+
+$query2 = "INSERT into marcacion(fecha,hora_inicio,hola_fin,docente_id, dia_id) VALUES (now(),
+ CURRENT_TIME(), CURRENT_TIME(), '$id_docente', WEEKDAY(now()))";
+$consulta = mysqli_query($conexion,$query2);
+echo "se creo la asistencia";
+
+header("location: index.php");
+
 
 // $q = "select id from docentes where usuario_id = '$usuario_id'";
 
