4f739a7639cd0eb047633364f28352cec2ca16f1
Lucas Saavedra <lucassaavedra50@gmail.com>
2021-04-22 11:47:11 -0300

    Consulta de detalle jornada y nueva bd

--- consultas.sql
+++ consultas.sql
@@ -1,3 +1,5 @@
 SELECT doc.nombre,cargo.nombre as Cargo,dj.hora_inicio,dj.hora_fin,dj.dia, j.fecha_inicio,j.fecha_fin FROM jornada as j INNER JOIN detalle_jornada as dj ON j.id = dj.id_jornada INNER JOIN cargo ON cargo.id = j.id_cargo INNER JOIN docentes as doc ON doc.id = cargo.docente_id
 
 INSERT into marcacion(fecha,hora_inicio,hola_fin,docente_id, dia_id) VALUES (now(), CURRENT_TIME(), CURRENT_TIME(), 3, WEEKDAY(now()))
+
+SELECT marcacion.dia_id,detalle_jornada.dia,marcacion.id as marcacion_id ,jornada.id as jornada_id, detalle_jornada.hora_inicio,detalle_jornada.hora_fin FROM marcacion,jornada inner join detalle_jornada ON jornada.id = detalle_jornada.id_jornada WHERE marcacion.fecha>= fecha_inicio and marcacion.fecha<=fecha_fin and marcacion.dia_id = detalle_jornada.dia
--- registrar-asistencia.php
+++ registrar-asistencia.php
@@ -1,21 +1,48 @@
-<?php 
+<?php
 require 'database.php';
 session_start();
 
 $usuario_id = $_SESSION['usuario_id'];
 
-$query ="select docentes.id from docentes, usuarios where usuario_id = '$usuario_id'";
-$consulta = mysqli_query($conexion,$query);
+$det_jornada = "SELECT detalle_jornada.id as det_jornada_id, detalle_jornada.hora_inicio,detalle_jornada.hora_fin FROM
+jornada inner join detalle_jornada ON jornada.id = detalle_jornada.id_jornada WHERE 
+now() >= fecha_inicio and
+now() <=fecha_fin and
+WEEKDAY(now()) = detalle_jornada.dia";
+$consulta = mysqli_query($conexion, $det_jornada);
 $array = mysqli_fetch_assoc($consulta);
-$id_docente= $array['id'];
+$det_jornada_id = $array['det_jornada_id'];
+
+$query = "select docentes.id from docentes, usuarios where usuario_id = '$usuario_id'";
+$consulta = mysqli_query($conexion, $query);
+$array = mysqli_fetch_assoc($consulta);
+$id_docente = $array['id'];
 
 
-$query2 = "INSERT into marcacion(fecha,hora_inicio,hola_fin,docente_id, dia_id) VALUES (now(),
- CURRENT_TIME(), CURRENT_TIME(), '$id_docente', WEEKDAY(now()))";
-$consulta = mysqli_query($conexion,$query2);
-echo "se creo la asistencia";
+$query2 = "INSERT into marcacion(fecha,hora_registro,docente_id, dia_id,detalle_jornada_id) VALUES (now(),
+ CURRENT_TIME(), '$id_docente', WEEKDAY(now()), $det_jornada_id )";
 
-header("location: index.php");
+if (mysqli_query($conexion, $query2)) {
+        $last_id = mysqli_insert_id($conexion);
+        echo "New record created successfully. Last inserted ID is: " . $last_id;
+} else {
+        echo "Error: " . $query2 . "<br>" . mysqli_error($conexion);
+}
+
+/* $q= 'SELECT TIMEDIFF(marcacion.hora_inicio,detalle_jornada.hora_fin) as cont FROM marcacion,detalle_jornada';
+$consulta = mysqli_query($conexion,$q);
+$con = mysqli_query($conexion,$q);
+while ($row = mysqli_fetch_assoc($consulta)) {
+  
+        echo "Row1";
+        echo $row['cont'];
+        echo "<br>"; */
+/*     
+} */
+
+
+
+
 
 
 // $q = "select id from docentes where usuario_id = '$usuario_id'";
