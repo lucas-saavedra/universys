793d4d5fdd45db1e5339528772152506c6fa2bb9
Lucas Saavedra <lucassaavedra50@gmail.com>
2021-06-18 11:29:14 -0300

    Se agregó vista de crear jornada

---
+++ crear_jornada.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Crear Jornada</title>
+    <style>
+        .containerForm {
+            display: flex;
+        
+        }
+    </style>
+</head>
+
+<body>
+            <h4 class="text-light p-3">UNIVERSYS</h4>
+            <div class="containerForm">
+                <form action="" method="POST">
+                    <label for="appt">Fecha de incio</label>
+                    <input type="date" id="date">
+                    <br>
+                    <label for="appt">Fecha de fin</label>
+                    <input type="date" id="date">
+                    <br>
+                    <label for="descripcion">Descripcion de la jornada</label>
+                    <input type="textarea" name="" id="">
+                    <button class="btn btn-danger" type="submit">Crear jornada</button>
+                </form>
+            </div>
+</body>
+
+</html>
