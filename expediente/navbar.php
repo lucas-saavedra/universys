71e88a7e77aba4f392c716d3c7824390b8ee00f8
Andreski <andres89may@gmail.com>
2021-06-24 14:56:53 -0300

    Agrego barra de navegacion

---
+++ expediente/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Universys</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#universys-nav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="universys-nav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="subir-documentacion.php">Documentación</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="crear-expediente.php">Expedientes</a>
+            </li>
+        </ul>
+    </div>
+</nav>
