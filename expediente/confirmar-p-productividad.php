a8be60e152e1619207fa7e60defd69e5e0786928
Andreski <andres89may@gmail.com>
2021-08-13 14:24:51 -0300

    P.prod: muestro por defecto la del mes actual

--- expediente/confirmar-p-productividad.php
+++ expediente/confirmar-p-productividad.php
@@ -1,21 +1,52 @@
 <?php include ("../includes/header.php");?>
 <?php include ("../includes/menu.php");?>
+<?php include ("./includes/consultas.php");
+
+$hoy = new DateTime('NOW');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST'){
+    $id_mes_actual = $hoy->format('n');
+    $mes = get_meses($conexion, $id_mes_actual)[0]['nombre'];
+    $anio = $hoy->format('Y');
+    $tipo_agente = "Docente";
+}
+else{
+    $mes = $_POST['mes'];
+    $anio = $_POST['anio'];
+    $tipo_agente=$_POST['select'];
+}
+
+
+?>
+
 <div class="container-fluid">
     <div class="row">
-        <div class="col-6 m-auto">
+        <div class="col-md-10 m-auto">
             <form action="" method="post"> 
-                <div class="form-group">
-                    <div class="row col text-center">
-                        <h2 class="col-md-8">Planilla de productividad</h2>
-                        <select name="select" class="form-control mr-sm-2 col-3">
-                            <option value="Docente">Docente </option>
-                            <option value="No docente">No docente </option>
-                        </select>
+                <div class="form-group mt-4">
+                    <div class="row mb-2">
+                        <div class="col text-center">
+                            <h2 class="">Planilla de productividad</h2>
+                        </div>
                     </div>
                     <div class="row col text-center">
-                        <input class="form-control mr-sm-2 col" type="search" name="mes" placeholder="Escriba el mes" aria-label="Search">
-                        <input class="form-control mr-sm-2 col" type="search" name="anio" placeholder="ingrese el año" aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0 col-md" name="planilla" type="submit">BUSCAR.</button>
+                        <select name="select" class="form-control mr-sm-2 col-3" required>
+                            <option value="Docente" <?=$tipo_agente == 'Docente' ? 'selected': ''?>>Docente</option>
+                            <option value="No docente" <?=$tipo_agente == 'No docente' ? 'selected': ''?>>No docente</option>
+                        </select>
+                        <select name="mes" class="form-control mr-sm-2 col-3" required>
+                            <option value="" selected disabled>Seleccione un mes</option>
+                            <?php foreach (get_meses($conexion) as $_mes): ?>
+                                <option 
+                                    value="<?=$_mes['nombre']?>"
+                                    <?=$_mes['nombre'] == $mes ? 'selected': ''?>
+                                >
+                                    <?=$_mes['nombre']?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                        <input class="form-control mr-sm-2 col" type="number" name="anio" placeholder="Ingrese el año" value="<?=$anio?>" required>
+                        <button class="btn btn-outline-success my-2 my-sm-0 d-inline-block" type="submit"><i class="fa fa-search"></i> BUSCAR</button>
                         <!--<a href="desc_exel.php" class="btn btn-outline-success">descargar</a> -->
                     </div>
                 </div>
@@ -23,17 +54,14 @@
         </div>
     </div>
     <?php
-        if (isset($_POST['planilla'] )){
-            $mes = $_POST['mes'];
-            $anio = $_POST['anio'];
-            $tipo_agente=$_POST['select'];
-            if ($tipo_agente == 'Docente' ){
-                $query_planilla ="SELECT  planilla_productividad_docente.id,planilla_productividad_docente.anio, m1.nombre FROM planilla_productividad_docente,(SELECT * FROM `mes` WHERE nombre= '$mes') as m1 ";
-                $result_planilla = mysqli_query($conexion,$query_planilla);
-            
-                while ($row_planilla = mysqli_fetch_array($result_planilla)){
-                    if ($row_planilla > 0){
-                    $planilla_id = $row_planilla['id']
+
+        if ($tipo_agente == 'Docente' ){
+            $query_planilla ="SELECT  planilla_productividad_docente.id,planilla_productividad_docente.anio, m1.nombre FROM planilla_productividad_docente,(SELECT * FROM mes WHERE nombre= '$mes') as m1 WHERE m1.id= planilla_productividad_docente.mes_id and anio='$anio'";
+            $result_planilla = mysqli_query($conexion,$query_planilla);
+        
+            while ($row_planilla = mysqli_fetch_array($result_planilla)){
+                if ($row_planilla > 0){
+                $planilla_id = $row_planilla['id']
     ?>
     <table class="table table-striped table-dark table-sm">
         <thead>
@@ -250,8 +278,6 @@
         </tbody>
     </table>
     <?php } } } }        
-    }else{
-    }
     ?>
 </div>
 
