727d347ed3143e734e60f93d804aca1acfecd917
Lucas Saavedra <lucassaavedra50@gmail.com>
2021-08-24 11:19:11 -0300

    Se agregó el rol admin

--- jornada/index.php
+++ jornada/index.php
@@ -1,7 +1,6 @@
 <?php
 include("navbar.php");
 
-
 if (!isset($_SESSION['agente'])) {
     header("Location: ../index.php ");
 }
@@ -13,6 +12,7 @@ $Object = new DateTime();
 $DateAndTime = $Object->format("h:i:s a");
 $fecha = date("Y-n-j");
 $fecha_string = (strtotime($fecha));
+
 ?>
 
 <div class="jumbotron jumbotron-fluid">
@@ -29,80 +29,16 @@ $fecha_string = (strtotime($fecha));
             echo '<div class="col-md-6">';
             $array_id = mysqli_fetch_assoc($result_docente);
             $result_jornadas = get_jornadas_docentes_hoy($conexion, $array_id['id']);
-            if (mysqli_num_rows($result_jornadas) !== 0) {
+            $result_jornadas_mesa = get_jornadasmesa_docentes_hoy($conexion, $array_id['id']);
+            if (mysqli_num_rows($result_jornadas_mesa) !== 0 || mysqli_num_rows($result_jornadas) !== 0) {
         ?>
                 <div class="card">
                     <div class="card-header text-center">Docente<form action="../backend/registrar-asistencia.php" method="POST" class="py-3">
                             <button class="btn btn-primary" type="submit">Registrar asistencia</button>
-                            <input type="hidden" id="time" name="tiempo" value="<?= $DateAndTime ?>"></label>
-                            <input type="hidden" name="fecha" value="<?= $fecha_string ?>"></label>
                         </form>
                     </div>
                     <div class="card-body">
-
-                        <div class="table-responsive rounded">
-                            <table class="table table-sm">
-                                <thead class="table-dark">
-                                    <tr>
-                                        <th scope="col">Inicio</th>
-                                        <th scope="col">Fin</th>
-                                        <th scope="col">Area</th>
-                                    </tr>
-                                </thead>
-                                <tbody id="listar_jornadas">
-                                    <?php
-
-                                    while ($row_jornadas = mysqli_fetch_array($result_jornadas)) {
-                                        $fecha_inicio = $row_jornadas['fecha_inicio'];
-                                        $fecha_fin    = $row_jornadas['fecha_fin'];
-                                        if (strtotime($fecha_inicio) < strtotime($fecha) and strtotime($fecha_fin) > strtotime($fecha)) {
-                                    ?>
-                                            <tr>
-                                                <td> <?php echo date("H:i", strtotime($row_jornadas['hora_inicio'])) . ' hs' ?> </td>
-                                                <td> <?php echo date("H:i", strtotime($row_jornadas['hora_fin'])) . ' hs' ?> </td>
-                                                <td> <?php echo $row_jornadas['catedra'] ?> | <?php echo $row_jornadas['carrera'] ?> </td>
-                                            </tr>
-                                        <?php  } ?>
-                                    <?php  } ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            <?php } else {
-                echo '<div class="alert alert-primary" role="alert">
-                        ¡No tiene jornadas como docente para hoy!
-                        </div>';
-            } ?>
-                 </div>
-     <?php    } ?>
-
-
-        <?php
-        $query_no_docente = "SELECT *FROM no_docente WHERE persona_id='$persona_id'";
-        $result_no_docente = mysqli_query($conexion, $query_no_docente);
-        if (mysqli_num_rows($result_no_docente) !== 0) {
-            echo '<div class="col-md-6">';
-            $array_id = mysqli_fetch_assoc($result_no_docente);
-            $result_jornadas = get_jornadas_no_docentes_hoy($conexion, $array_id['id']);
-            if (mysqli_num_rows($result_jornadas) !== 0) {
-
-        ?>
-                <div class="card">
-                    <div class="card-header text-center">No docente <form action="../expediente/registrar-asistencia_no_docente.php" method="POST" class="py-3">
-                            <button class="btn btn-primary" type="submit">Registrar asistencia</button>
-                            <input type="hidden" id="time" name="tiempo" value="<?= $DateAndTime ?>"></label>
-                            <input type="hidden" name="fecha" value="<?= $fecha_string ?>"></label>
-                        </form>
-                    </div>
-                    <div class="card-body">
-
-                        <?php
-
-                        if (mysqli_num_rows($result_jornadas) !== 0) {
-
-                        ?>
-
+                        <?PHP if (mysqli_num_rows($result_jornadas_mesa) !== 0) { ?>
                             <div class="table-responsive rounded">
                                 <table class="table table-sm">
                                     <thead class="table-dark">
@@ -110,45 +46,175 @@ $fecha_string = (strtotime($fecha));
                                             <th scope="col">Inicio</th>
                                             <th scope="col">Fin</th>
                                             <th scope="col">Area</th>
+                                            <th scope="col">Tipo</th>
+                                            <th scope="col">Estado</th>
                                         </tr>
                                     </thead>
-                                    <tbody id="listar_jornadas">
+                                    <tbody>
                                         <?php
 
-
-                                        while ($row_jornadas = mysqli_fetch_array($result_jornadas)) {
+                                        while ($row_jornadas = mysqli_fetch_array($result_jornadas_mesa)) {
                                             $fecha_inicio = $row_jornadas['fecha_inicio'];
                                             $fecha_fin    = $row_jornadas['fecha_fin'];
-
                                             if (strtotime($fecha_inicio) < strtotime($fecha) and strtotime($fecha_fin) > strtotime($fecha)) {
-
                                         ?>
                                                 <tr>
                                                     <td> <?php echo date("H:i", strtotime($row_jornadas['hora_inicio'])) . ' hs' ?> </td>
                                                     <td> <?php echo date("H:i", strtotime($row_jornadas['hora_fin'])) . ' hs' ?> </td>
-                                                    <td> <?php echo $row_jornadas['area'] ?></td>
-                                                </tr>
+                                                    <td> <?php echo $row_jornadas['carrera_nombre'] ?></td>
+                                                    <td> <?php echo '<span class="badge badge-primary">Mesa de examen</span>' ?></td>
+                                                    <td> <?php echo '<span class="badge badge-success">Activa</span>' ?></td>
 
+                                                </tr>
                                             <?php  } ?>
                                         <?php  } ?>
-                                    <?php  } ?>
-
                                     </tbody>
                                 </table>
                             </div>
+                        <?php } ?>
+                        <?PHP if (mysqli_num_rows($result_jornadas) !== 0) { ?>
+                            <div class="table-responsive rounded">
+                                <table class="table table-sm">
+                                    <thead class="table-dark">
+                                        <tr>
+                                            <th scope="col">Inicio</th>
+                                            <th scope="col">Fin</th>
+                                            <th scope="col">Area</th>
+                                            <th scope="col">Estado</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+
+
+                                        if (mysqli_num_rows($result_jornadas_mesa) !== 0) {
+                                            $estado = 'Inactiva';
+                                            $tipo_estado = 'secondary';
+                                        } else {
+
+                                            $estado = 'Activa';
+                                            $tipo_estado = 'success';
+                                        }
+
+                                        while ($row_jornadas = mysqli_fetch_array($result_jornadas)) {
+                                            $fecha_inicio = $row_jornadas['fecha_inicio'];
+                                            $fecha_fin    = $row_jornadas['fecha_fin'];
+                                            if (strtotime($fecha_inicio) < strtotime($fecha) and strtotime($fecha_fin) > strtotime($fecha)) {
+                                        ?>
+                                                <tr>
+                                                    <td> <?php echo date("H:i", strtotime($row_jornadas['hora_inicio'])) . ' hs' ?> </td>
+                                                    <td> <?php echo date("H:i", strtotime($row_jornadas['hora_fin'])) . ' hs' ?> </td>
+                                                    <td> <?php echo $row_jornadas['catedra'] ?> | <?php echo $row_jornadas['carrera'] ?> </td>
+                                                    <td> <?php $Object = new DateTime();
+                                                            $hora_actual = $Object->format("H:i");
+                                                            if (strtotime($row_jornadas['hora_inicio']) < strtotime($hora_actual) and strtotime($row_jornadas['hora_fin']) > strtotime($hora_actual)) {
+                                                                $estado = 'Activa';
+                                                                $tipo_estado = 'success';
+                                                            } else {
+                                                                $estado = 'Inactiva';
+                                                                $tipo_estado = 'secondary';
+                                                            }
+                                                            echo '<span class="badge badge-' . $tipo_estado . '">' . $estado . '</span>' ?></td>
+
+                                                </tr>
+                                            <?php  } ?>
+                                        <?php  } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        <?php } ?>
                     </div>
                 </div>
             <?php } else {
-                echo '
+                echo '<div class="alert alert-primary" role="alert">
+                        ¡No tiene jornadas como docente para hoy!
+                        </div>';
+            } ?>
+    </div>
+<?php    } ?>
+
+
+<?php
+$query_no_docente = "SELECT *FROM no_docente WHERE persona_id='$persona_id'";
+$result_no_docente = mysqli_query($conexion, $query_no_docente);
+if (mysqli_num_rows($result_no_docente) !== 0) {
+    echo '<div class="col-md-6">';
+    $array_id = mysqli_fetch_assoc($result_no_docente);
+    $result_jornadas = get_jornadas_no_docentes_hoy($conexion, $array_id['id']);
+    if (mysqli_num_rows($result_jornadas) !== 0) {
+
+?>
+        <div class="card">
+            <div class="card-header text-center">No docente <form action="../expediente/registrar-asistencia_no_docente.php" method="POST" class="py-3">
+                    <button class="btn btn-primary" type="submit">Registrar asistencia</button>
+                  
+                </form>
+            </div>
+            <div class="card-body">
+
+                <?php
+
+                if (mysqli_num_rows($result_jornadas) !== 0) {
+
+                ?>
+
+                    <div class="table-responsive rounded">
+                        <table class="table table-sm">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th scope="col">Inicio</th>
+                                    <th scope="col">Fin</th>
+                                    <th scope="col">Area</th>
+                                    <th scope="col">Estado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+
+
+                                while ($row_jornadas = mysqli_fetch_array($result_jornadas)) {
+                                    $fecha_inicio = $row_jornadas['fecha_inicio'];
+                                    $fecha_fin    = $row_jornadas['fecha_fin'];
+
+                                    if (strtotime($fecha_inicio) < strtotime($fecha) and strtotime($fecha_fin) > strtotime($fecha)) {
+
+                                ?>
+                                        <tr>
+                                            <td> <?php echo date("H:i", strtotime($row_jornadas['hora_inicio'])) . ' hs' ?> </td>
+                                            <td> <?php echo date("H:i", strtotime($row_jornadas['hora_fin'])) . ' hs' ?> </td>
+                                            <td> <?php echo $row_jornadas['area'] ?></td>
+                                            <td> <?php $Object = new DateTime();
+                                                    $hora_actual = $Object->format("H:i");
+                                                    if (strtotime($row_jornadas['hora_inicio']) < strtotime($hora_actual) and strtotime($row_jornadas['hora_fin']) > strtotime($hora_actual)) {
+                                                        $estado = 'Activa';
+                                                        $tipo_estado = 'success';
+                                                    } else {
+                                                        $estado = 'Inactiva';
+                                                        $tipo_estado = 'secondary';
+                                                    }
+                                                    echo '<span class="badge badge-' . $tipo_estado . '">' . $estado . '</span>' ?></td>
+                                        </tr>
+
+                                    <?php  } ?>
+                                <?php  } ?>
+                            <?php  } ?>
+
+                            </tbody>
+                        </table>
+                    </div>
+            </div>
+        </div>
+    <?php } else {
+        echo '
                 <div class="alert alert-primary" role="alert">
                 ¡No tiene jornadas como no docente para hoy!
                 </div>
                 ';
-            } ?>
-        <?php echo '</div>';
-        }  ?>
+    } ?>
+<?php echo '</div>';
+}  ?>
 
-    </div>
+</div>
 </div>
 
 <?php include("../includes/footer.php") ?>
