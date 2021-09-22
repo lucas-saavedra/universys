442812d10a60e0edfb21be30291d89c11123e836
alerellan <alerellan@gmail.com>
2021-09-22 11:18:35 -0300

    boton de elimiacion de inasistencias en los no docentes

--- expediente/eliminar_inasis.php
+++ expediente/eliminar_inasis.php
@@ -8,4 +8,10 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST' || !isset($_POST['id_docente'])) heade
         
     header("Location:crear-expediente-sin-aviso.php?del_expdte_id={$_POST['id_docente']}");
     
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST' || !isset($_POST['id_no_docente'])) header("Location:crear-expediente-sin-aviso.php");
+
+    $result = mysqli_query($conexion, "DELETE FROM inasistencia_sin_aviso_no_docente WHERE id={$_POST['id_no_docente']}");
+        
+    header("Location:crear-expediente-sin-aviso.php?del_expdte_id={$_POST['id_no_docente']}");
 ?>
